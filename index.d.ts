c3b80f5e33fb1807e1eac92655caf53104678de5
ntnyq <ntnyq13@gmail.com>
2019-09-09 21:24:41 +0800

    fix: optional verbose option

--- index.d.ts
+++ index.d.ts
@@ -3,14 +3,14 @@ import { Transform } from 'stream'
 /**
  * Returns a stream that compiles Vinyl files as HTML
  */
-declare function GulpFormatHtml (options: GulpFormatHtml.Options): Transform
+declare function GulpFormatHtml (options: GulpFormatHtml.Options): Transform;
 
 declare namespace GulpFormatHtml {
   interface Options {
     /**
      * Display name of file from stream that is being formatting
      */
-    verbose: boolean;
+    verbose?: boolean;
   }
 }
 
